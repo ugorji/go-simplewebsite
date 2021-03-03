fdc6c1f33ae7323ade5ff93adefdaad647f62a37
Ugorji Nwoke <ugorji@gmail.com>
2021-03-03 12:13:48 -0500

    updates README for command: simplewebsite

---
+++ cmd/simplewebsite/README.md
@@ -0,0 +1,21 @@
+# Documentation for simplewebsite command.
+
+To install:
+
+```
+go get github.com/ugorji/go-simplewebsite/cmd/simplewebsite
+```
+
+# Package Documentation
+
+simplewebsite is a fast, flexible static site generator or dynamic file
+server.
+
+It is a modern, lean, fast website engine for typical websites, with mostly
+static files and some dynamic content. It uses templates, markdown files and
+other static assets to providesimplewebsite
+
+Usage:
+
+```
+    simplewebsite -d basedir
---
+++ cmd/simplewebsite/doc.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2012-2020 Ugorji Nwoke. All rights reserved.
+// Use of this source code is governed by a MIT license found in the LICENSE file.
+
+// simplewebsite is a fast, flexible static site generator or dynamic file server.
+//
+// It is a modern, lean, fast website engine for typical websites, with mostly static files and some dynamic content.
+// It uses templates, markdown files and other static assets to providesimplewebsite 
+// 
+// Usage:
+//   simplewebsite -d basedir
+package main
+
